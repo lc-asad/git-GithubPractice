99bf082c89fd89f18f63ccac2a6290a8db2c9c8b
Asad <asad_622@hotmail.com>
2021-08-22 22:41:38 +0500

    Local changes added to test conflict with the remote

--- git-GithubPractice/AppDelegate.swift
+++ git-GithubPractice/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-// A commit is added locally to test conflict
+// A commit is added locally to test conflict, again made from local too
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
